5da1273700a55a9edbdbb08a1384d45ae04b950d
rahul kumar <2020pgcaca67@gmail.com>
2024-05-13 23:13:50 +0530

    new paragraph added in practice2.kt file

--- src/main/kotlin/practice2.kt
+++ src/main/kotlin/practice2.kt
@@ -2,3 +2,7 @@
 /* hello how are you i am creating this file in master branch and i will create
 another same file in another branch anf will merge it let see what happen
  */
+fun main()
+{
+    println("I have created this file in new branch master1")
+}
